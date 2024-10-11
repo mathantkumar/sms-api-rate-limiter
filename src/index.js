8138195146f50de75c051c82314f4aed029ed457
mathantkumar <mathankumar2001@gmail.com>
2024-10-12 00:22:41 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://sms-dashboard-ten.vercel.app",
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
   })
